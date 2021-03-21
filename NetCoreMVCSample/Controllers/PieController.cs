79a7f9df96c4a5ec25cae5e9899cba9c330f0ad7
anandprakashdubey <dby.anandprakash@gmail.com>
2021-03-21 18:18:42 +0000

    Added View Components

--- NetCoreMVCSample/Controllers/PieController.cs
+++ NetCoreMVCSample/Controllers/PieController.cs
@@ -19,12 +19,28 @@ namespace NetCoreMVCSample.Controllers
             this._categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            PiesListViewModel pieListViewModel = new PiesListViewModel();
-            pieListViewModel.Pies = _pieRepository.AllPies;
-            pieListViewModel.CurrentCategory = "Cheese cake";
-            return View(pieListViewModel);
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         public IActionResult Details(int id)
